03fcc3405fd5fc2e585a0b7e45170311c3872cba
Blake <bmichalzik@gmail.com>
2020-08-01 13:19:59 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/LetterComplier.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LetterComplier.dir/main.c.obj"
+  "LetterComplier.exe"
+  "LetterComplier.exe.manifest"
+  "LetterComplier.lib"
+  "LetterComplier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LetterComplier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
